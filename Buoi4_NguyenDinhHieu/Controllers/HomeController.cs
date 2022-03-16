3d550c92724cc457eb29b561fe76d8d9f73f2fb9
Hieu <hieu01672549298@gmail.com>
2022-03-16 15:41:00 +0700

    Add project files.

---
+++ Buoi4_NguyenDinhHieu/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Buoi4_NguyenDinhHieu.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Buoi4_NguyenDinhHieu.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index( int ? page)
+        {
+            if (page == null) page = 1;
+            var all_sach = (from ss in data.Sach1s select ss).OrderBy(m=>m.masach);
+            int PageSize = 3;
+            int PageNum = page ?? 1;
+            return View(all_sach.ToPagedList(PageNum,PageSize));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
