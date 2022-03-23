5787e094fe41227060acff94f56b5e00c808d981
Hieupmv2911 <101702088+Hieupmv2911@users.noreply.github.com>
2022-03-23 16:54:35 +0700

    Add files via upload

---
+++ Buoi4_NguyenDinhHieu/Controllers/NguoiDungController.cs
@@ -0,0 +1,113 @@
+﻿using Buoi4_NguyenDinhHieu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Buoi4_NguyenDinhHieu.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: Nguoidung
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(FormCollection collection, KhachHang kh)
+        {
+            var hoten = collection["hoten"];
+            var tendn = collection["tendangnhap"];
+            var matkhau = collection["matkhau"];
+            var nhaplaimatkhau = collection["MatKhauXacNhan"];
+            var email = collection["email"];
+            var diachi = collection["diachi"];
+            var dienthoai = collection["dienthoai"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["ngaysinh"]);
+            if (String.IsNullOrEmpty(hoten))
+            {
+                ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
+            }
+            else if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi3"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(nhaplaimatkhau))
+            {
+                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+            }
+            if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Loi5"] = "Email không được bỏ trống";
+            }
+            if (String.IsNullOrEmpty(dienthoai))
+            {
+                ViewData["Loi6"] = "Phải nhập điện thoại";
+            }
+            else
+            {
+                kh.hoten = hoten;
+                kh.tendangnhap = tendn;
+                kh.matkhau = matkhau;
+                kh.email = email;
+                kh.diachi = diachi;
+                kh.dienthoai = dienthoai;
+                kh.ngaysinh = DateTime.Parse(ngaysinh);
+                data.KhachHangs.InsertOnSubmit(kh);
+                data.SubmitChanges();
+                return RedirectToAction("DangNhap");
+
+            }
+            return this.DangKy();
+        }
+
+        [HttpGet]
+
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection, KhachHang kh)
+        {
+            var tendn = collection["TenDN"];
+            var matkhau = collection["MatKhau"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                kh = data.KhachHangs.SingleOrDefault(n => n.tendangnhap == tendn && n.matkhau == matkhau);
+                if (kh != null)
+                {
+                    ViewBag.ThongBao = "Đăng nhập thành công!!!";
+                    Session["TaiKhoan"] = kh;
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng!!!";
+                }
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
